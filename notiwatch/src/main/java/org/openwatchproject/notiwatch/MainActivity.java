9e94fc0657392f2a7c645d20659fa648e4c25fa2
Iscle <albertiscle9@gmail.com>
2021-09-21 16:29:35 +0200

    Initial commit

---
+++ notiwatch/src/main/java/org/openwatchproject/notiwatch/MainActivity.java
@@ -0,0 +1,14 @@
+package org.openwatchproject.notiwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
